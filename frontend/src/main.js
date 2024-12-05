472a764c91bb46d24731e7a3f1418c84319cb34c
Exilus-g <yisisclan@gmail.com>
2024-12-05 02:19:03 -0600

    d

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,25 +1,30 @@
-import './style.css'
-import { createApp,markRaw } from 'vue'
-import { createPinia } from 'pinia'
-import { DiagramComponent, SymbolPaletteComponent } from '@syncfusion/ej2-vue-diagrams';
-import { ButtonComponent } from '@syncfusion/ej2-vue-buttons';
+import "./style.css";
+import { createApp, markRaw } from "vue";
+import { createPinia } from "pinia";
+import {
+  DiagramComponent,
+  SymbolPaletteComponent,
+} from "@syncfusion/ej2-vue-diagrams";
+import { ButtonComponent } from "@syncfusion/ej2-vue-buttons";
 
-import App from './App.vue'
-import router from './router'
-import { registerLicense} from '@syncfusion/ej2-base'
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NDaF1cX2hIYVdpR2Nbek5zflVGallXVAciSV9jS3pSdEVmWXxfdXBcQmJeWQ==')
+import App from "./App.vue";
+import router from "./router";
+import { registerLicense } from "@syncfusion/ej2-base";
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1NDaF1cX2hIYVdpR2Nbek5zflVGallXVAciSV9jS3pSdEVmWXxfdXBcQmJeWQ=="
+);
 
-const app = createApp(App)
-const pinia = createPinia()
-app.component('ejs-diagram', DiagramComponent);
-app.component('ejs-symbolpalette', SymbolPaletteComponent);
-app.component('ejs-button', ButtonComponent);
+const app = createApp(App);
+const pinia = createPinia();
+app.component("ejs-diagram", DiagramComponent);
+app.component("ejs-symbolpalette", SymbolPaletteComponent);
+app.component("ejs-button", ButtonComponent);
 
-pinia.use(({store}) => {
-    store.router = markRaw(router)
-})
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
 
-app.use(pinia)
-app.use(router)
+app.use(pinia);
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
